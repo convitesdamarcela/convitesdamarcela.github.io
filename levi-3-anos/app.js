47da30f24013e81cc28bde1cb1b18ae31207b0bf
ConvitesdaMarcela <convitesdamarcela@gmail.com>
2026-01-22 00:05:42 -0300

    Add files via upload

---
+++ levi-3-anos/app.js
@@ -0,0 +1,1 @@
+fetch('experience.json').then(r=>r.json()).then(cfg=>{fetch(cfg.tracking.url+'?site='+cfg.ids.site_id).then(r=>r.json()).then(s=>{if(s.status==='BLOCKED'){document.body.innerHTML='<h2>Experiência indisponível</h2>';}});});
